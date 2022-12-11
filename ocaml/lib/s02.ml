a4872ba6afc51d6a34ee806c24e77d20e71a5179
Kim Nguyễn <kim.nguyen@lri.fr>
2022-12-11 19:00:59 +0100

    Simplify s02.ml by using mod 3 arithmetic.

--- ocaml/lib/s02.ml
+++ ocaml/lib/s02.ml
@@ -1,24 +1,21 @@
-type move = Rock | Paper | Scissors
-
-let score = function Rock -> 1 | Paper -> 2 | Scissors -> 3
+let _ROCK = 0
+let _PAPER = 1
+let _SCISSORS = 2
+let next n = (n + 1) mod 3
+let prev n = (n + 2) mod 3
 
 let of_letter = function
-  | "A" | "X" -> Rock
-  | "B" | "Y" -> Paper
-  | "C" | "Z" -> Scissors
+  | "A" | "X" -> _ROCK
+  | "B" | "Y" -> _PAPER
+  | "C" | "Z" -> _SCISSORS
   | s -> failwith ("Invalid move: '" ^ s ^ "'")
 
-let compare m1 m2 =
-  match (m1, m2) with
-  | Rock, Paper | Scissors, Rock | Paper, Scissors -> -1
-  | Paper, Rock | Rock, Scissors | Scissors, Paper -> 1
-  | Rock, Rock | Scissors, Scissors | Paper, Paper -> 0
+let score_move op my =
+  1 + my + if next op = my then 6 else if op = my then 3 else 0
 
 let decide1 old_score op_move my_move_s =
   let my_move = of_letter my_move_s in
-  let c = compare op_move my_move in
-  let points = if c = 0 then 3 else if c < 0 then 6 else 0 in
-  old_score + points + score my_move
+  old_score + score_move op_move my_move
 
 let solve decide () =
   let total =
@@ -33,33 +30,15 @@ let solve decide () =
 let name = "02_part1"
 let () = Solution.register name (solve decide1)
 
-type result = Draw | Win | Loose
-
-let r_of_letter = function
-  | "X" -> Loose
-  | "Y" -> Draw
-  | "Z" -> Win
-  | s -> failwith ("Invalid result: '" ^ s ^ "'")
-
 let decide2 old_score op_move my_move_s =
-  let r = r_of_letter my_move_s in
   let my_move =
-    match r with
-    | Win -> (
-        match op_move with
-        | Rock -> Paper
-        | Paper -> Scissors
-        | Scissors -> Rock)
-    | Draw -> op_move
-    | Loose -> (
-        match op_move with
-        | Paper -> Rock
-        | Scissors -> Paper
-        | Rock -> Scissors)
+    match my_move_s with
+    | "X" -> prev op_move
+    | "Y" -> op_move
+    | "Z" -> next op_move
+    | s -> failwith ("Invalid result: '" ^ s ^ "'")
   in
-  let c = compare op_move my_move in
-  let points = if c = 0 then 3 else if c < 0 then 6 else 0 in
-  old_score + points + score my_move
+  old_score + score_move op_move my_move
 
 let name = "02_part2"
 let () = Solution.register name (solve decide2)

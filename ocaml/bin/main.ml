58e69dbaf2c94127c3e7e12f3d5eeb71c373a1cd
Kim Nguyễn <kim.nguyen@lri.fr>
2022-12-21 22:16:23 +0100

    Set long default margins.

--- ocaml/bin/main.ml
+++ ocaml/bin/main.ml
@@ -1,5 +1,12 @@
 open Aoc2022
 
+let () =
+  let open Format in
+  pp_set_margin err_formatter 2000;
+  pp_set_max_indent err_formatter 2000;
+  pp_set_margin std_formatter 2000;
+  pp_set_max_indent std_formatter 2000
+
 let main () =
   if Array.length Sys.argv < 2 then begin
     Format.printf "Usage: %s <problem>@\n" Sys.argv.(0);
@@ -12,7 +19,9 @@ let main () =
     exit 0
   end;
   match Solution.get Sys.argv.(1) with
-  None -> Format.printf "Error: unknown solution %s@\n" arg; exit 2
-  | Some f -> f()
+  | None ->
+      Format.printf "Error: unknown solution %s@\n" arg;
+      exit 2
+  | Some f -> f ()
 
 let () = main ()
